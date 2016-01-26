fe750ff93d09b42a965a02ed307d1914209b2aae
Akash Vartak <akashvartak.1995@gmail.com>
2016-01-26 14:31:30 +0530

    updated portfolio

--- dbconnect.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect('mysql5.000webhost.com','a9098406_akashv','akashvartak1995','a9098406_dbtest');
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
